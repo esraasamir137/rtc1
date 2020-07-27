906a252df9735af6aca9a6343e704da188bbcc2a
fatma morsy <fatmamorsy400@gmail.com>
2020-07-27 16:01:28 +0200

    update

--- rtcc.js
+++ rtcc.js
@@ -4,14 +4,14 @@ function doGet(e) {
     
     return HtmlService.createHtmlOutputFromFile("Trainer");
   }
-  function userClicked(name,email,tel1,tel2,tel3,tel4,tel5,tel6) {
+  function userClicked(userinfo) {
     
         var url="https://docs.google.com/spreadsheets/d/1UST1IF3UWC4Gig-dLtDrjEd_jmDTBMPfMUrY-y4-OtE/edit#gid=0";
   
          var ss=SpreadsheetApp.openByUrl(url);
          var ws=ss.getSheetByName("Data");
     
-    ws.appendRow([new Date() name,email,tel1,tel2,tel3,tel4,tel5,tel6]);
+    ws.appendRow([new Date(),userinfo.name,userinfo.email, userinfo.tel1, userinfo.tel2,userinfo.tel3,userinfo.tel4, userinfo.tel5, userinfo.tel6]);
 
   //   Logger.log(name,email,tel1,tel2,tel3,tel4,tel5,tel6+ " clicked the Button");
   }
