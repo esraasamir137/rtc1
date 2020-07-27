c1c1147e6ebf74d64b29861a3860f521b9131905
fatma morsy <fatmamorsy400@gmail.com>
2020-07-27 17:06:37 +0200

    update

--- rtcc.js
+++ rtcc.js
@@ -7,7 +7,7 @@ function doGet(e) {
   function userClicked(userinfo) {
     
         var url="https://docs.google.com/spreadsheets/d/1UST1IF3UWC4Gig-dLtDrjEd_jmDTBMPfMUrY-y4-OtE/edit#gid=0";
-  
+
          var ss=SpreadsheetApp.openByUrl(url);
          var ws=ss.getSheetByName("Data");
     
